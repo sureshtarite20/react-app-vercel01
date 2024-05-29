824ce3fcad40a484db0d1592cb5da65c3a7e45bf
sureshtarite <sureshtarite20@gmail.com>
2024-05-29 15:02:45 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.THIS IS TESTING FOR vercel deployment..........
+          This is first VERCEL DEPLOYMENT
         </p>
         <a
           className="App-link"
