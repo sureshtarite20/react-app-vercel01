0c405fbd3f57830ad62af89e08780afd73a44077
sureshtarite20 <79962213+sureshtarite20@users.noreply.github.com>
2024-05-30 22:55:23 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          --------- VERCEL DEPLOYMENT01====================
+          --------- VERCEL deployment01====================
         </p>
         <a
           className="App-link"
